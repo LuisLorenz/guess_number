97f24032c299d16d361e5cdde439b9cdf34c91e5
Luis Lorenzs <luislorenz7@gmail.com>
2024-07-08 19:58:48 +0200

    start defining concept

---
+++ guess_number.py
@@ -0,0 +1,11 @@
+import random
+
+def game():
+
+    # introduction
+    print('''Welcome ... 
+          are you ready to guess the number?
+          or do you want the computer to guess your number?
+          ''')
+        # implement flow writing
+        # input 
