55e3d57a678844c5e499b62c87edae1cda245488
Luis Lorenzs <luislorenz7@gmail.com>
2024-07-11 20:29:48 +0200

    implementation foundation playstyle 1

--- basic_code.py/random_choice.py
+++ basic_code.py/random_choice.py
@@ -1,9 +1,7 @@
 import random
 
-
-
 x = -1
 y = 1
-random_integer = random.randint(x, y)
+random_integer = random.randint(x, y) # random integer 
 
 print(random_integer)
--- guess_number.py
+++ guess_number.py
@@ -5,9 +5,8 @@ def game():
     # introduction
     print('''
 Welcome ... 
-Are you ready to guess THE NUMBER??? (type "1")
-Or do you want to CHALLENGE the computer to guess your number? (type "2")
-          ''')
+Are you ready to guess THE NUMBER??? (insert: "1")
+Or do you want to CHALLENGE the computer to guess your number? (insert: "2")''')
     
         # implement flow writing
         # input 
@@ -18,8 +17,35 @@ Or do you want to CHALLENGE the computer to guess your number? (type "2")
     # playstlye 1
     # random number in a certain range
     # boarders can be defined through difficulty level 
-    x = -10 
-    y = 10 
+    
+    difficulty = None
+
+    while difficulty == None:
+      difficulty = int(input('''
+Please insert the level of difficulty: 
+  Easy: "1"
+  Medium: "2"
+  Hard: "3"
+  -> '''))
+      
+      x = None
+      y = None
+
+      if difficulty == 1: 
+        x = -10 
+        y = 10   
+      elif difficulty == 2:
+        x = -1000
+        y = 1000 
+      elif difficulty == 3:
+        x = -1000000
+        y = 1000000
+      else:
+        print('Your input was invalid. Please try again!')
+        difficulty = None 
+
+    print(f'The searched number is between {x} and {y} including these numbers.')
+
     searched_number = random.randint(x, y)
     # while loop that keeps answers the user to the guess
     guess = None 
@@ -30,7 +56,7 @@ Or do you want to CHALLENGE the computer to guess your number? (type "2")
         elif guess > searched_number:
           print('Your guess was too high.')
         elif guess == searched_number:
-          print(f'Your guess was right. The searched number was {searched_number}!')
+          print(f'Your guess was right. The searched number was "{searched_number}"!')
         else:
           print('Your guess was invalid. Please try again!')
     # playstyle 2 
